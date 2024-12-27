b49ee0335668a488af5000bb5ef65b0141f85597
Ugyen Jigten <ujigten@gmail.com>
2024-12-27 12:32:57 +0600

    Update gitignore, run all solutions

--- src/main.rs
+++ src/main.rs
@@ -2,44 +2,44 @@ mod solutions;
 
 fn main() {
     println!("Advent of code 2024!");
-    //solutions::day1::solve_a();
-    //solutions::day1::solve_b();
+    solutions::day1::solve_a();
+    solutions::day1::solve_b();
 
-    //solutions::day2::solve_a();
-    //solutions::day2::solve_b();
+    solutions::day2::solve_a();
+    solutions::day2::solve_b();
 
-    //solutions::day3::solve_a();
-    //solutions::day3::solve_b();
+    solutions::day3::solve_a();
+    solutions::day3::solve_b();
 
-    //solutions::day4::solve_a();
-    //solutions::day4::solve_b();
+    solutions::day4::solve_a();
+    solutions::day4::solve_b();
 
-    //solutions::day5::solve_a();
-    //solutions::day5::solve_b();
+    solutions::day5::solve_a();
+    solutions::day5::solve_b();
 
-    //solutions::day6::solve_a();
-    //solutions::day6::solve_b();
+    solutions::day6::solve_a();
+    solutions::day6::solve_b();
 
-    //solutions::day7::solve_a();
-    //solutions::day7::solve_b();
+    solutions::day7::solve_a();
+    solutions::day7::solve_b();
 
-    //solutions::day8::solve_a();
-    //solutions::day8::solve_b();
+    solutions::day8::solve_a();
+    solutions::day8::solve_b();
 
-    //solutions::day9::solve_a();
-    //solutions::day9::solve_b();
+    solutions::day9::solve_a();
+    solutions::day9::solve_b();
 
-    //solutions::day10::solve_a();
+    solutions::day10::solve_a();
 
-    //solutions::day11::solve_a();
-    //solutions::day11::solve_b();
+    solutions::day11::solve_a();
+    solutions::day11::solve_b();
 
-    // solutions::day12::solve_a();
-    // solutions::day12::solve_b();
+    solutions::day12::solve_a();
+    solutions::day12::solve_b();
 
-    // solutions::day13::solve_a();
-    // solutions::day13::solve_b();
+    solutions::day13::solve_a();
+    solutions::day13::solve_b();
 
-    // solutions::day14::solve_a();
+    solutions::day14::solve_a();
     solutions::day14::solve_b();
 }
